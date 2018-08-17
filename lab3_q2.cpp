eed57183f17298cea2da500eeceddd9680fd0f2c
pradyotpsahoo <42238958+pradyotpsahoo@users.noreply.github.com>
2018-08-17 12:24:01 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,18 @@
+//Library
+#include<iostream>
+using namespace std;
+int main (){
+int a=80;
+int b=10;
+int sum = a+b;
+int difference= a-b;
+int multiply= a*b;
+int divide=a/b;
+int mod = a%b;
+cout<<"sum of a and b is " << sum<<endl;
+cout<<"difference of a and b is " <<difference<<endl;
+cout<<"mulltiplication of a and b is " <<multiply<<endl;
+cout<<"division of a and b is " <<divide<<endl;
+cout << "mod of 80 and 10 is " << mod << endl; 
+return 0;
+}
